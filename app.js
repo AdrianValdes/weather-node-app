558c0cbcfbc4ab4ed9b6bcc02b76b78b669c68c2
AdrianValdes <adrianvaldeshdz@gmail.com>
2020-11-24 16:42:47 +0100

    Added geoLocation

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const ACCESS_KEY = "2f3720fac48fe1812fed249b39d2eeea";
+const ACCESS_TOKEN =
+  "pk.eyJ1IjoiYWRyaWFudmFsZGVzIiwiYSI6ImNraHcybW56bTFzcmwyc2t6aG00ZzBtYXQifQ.TMNph6be1vXRvkUVjam8VQ";
 const request = require("request");
 
 const url =
@@ -10,4 +12,12 @@ request({ url, json: true }, (error, response) => {
   console.log(
     `The weather is ${weather_descriptions[0]}. It is currently ${temperature} degrees out. There is ${precip}% chance of rain`
   );
+});
+
+const urlGeo = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${ACCESS_TOKEN}&limit=1`;
+
+request({ url: urlGeo, json: true }, (error, response) => {
+  const data = response.body.features[0];
+  let { text, center } = data;
+  console.log(`The latitude is ${center[1]} and the longitude is ${center[0]}`);
 });
