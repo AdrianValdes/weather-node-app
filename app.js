90c9bed9131f2c0d6db53b3405a0ecbbefcdf789
AdrianValdes <adrianvaldeshdz@gmail.com>
2020-11-24 15:12:25 +0100

    Working with weatherstack api and the request npm module

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-console.log("Starting");
-setTimeout(() => {
-  console.log("2 sec");
-}, 1000);
-setTimeout(() => {
-  console.log("0 sec");
-}, 0);
-console.log("Ending");
+const ACCESS_KEY = "2f3720fac48fe1812fed249b39d2eeea";
+const request = require("request");
+
+const url =
+  "http://api.weatherstack.com/current?access_key=2f3720fac48fe1812fed249b39d2eeea&query=37.8267,-122.4233&units=m";
+
+request({ url, json: true }, (error, response) => {
+  const data = response.body.current;
+  let { weather_descriptions, temperature, precip } = data;
+  console.log(
+    `The weather is ${weather_descriptions[0]}. It is currently ${temperature} degrees out. There is ${precip}% chance of rain`
+  );
+});
